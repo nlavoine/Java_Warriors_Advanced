73b8addf54726bf4bbc6a16db9dae056f2b094ff
nicolas lavoine <nlavoine.dev@gmail.com>
2019-11-08 16:28:22 +0100

    Added post games

--- src/main/java/warriors/client/web/ClientWeb.java
+++ src/main/java/warriors/client/web/ClientWeb.java
@@ -7,18 +7,14 @@ import io.vavr.jackson.datatype.VavrModule;
 import warriors.contracts.WarriorsAPI;
 import warriors.engine.Warriors;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
-import java.util.Scanner;
 
 
 public class ClientWeb {
 
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        Web web = new Web(sc);
 
 
 
@@ -29,22 +25,46 @@ public class ClientWeb {
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
 
 
-
         /////////////////////////////////////////////////////////////////////////////
         //ROUTER
 
+        //List of heroes
         server.createContext("/api/heroes", (exchange -> {
             String respText = mapper.writeValueAsString(warriors.availableHeroes());
             doResponse("GET", exchange, respText);
         }));
 
         //-------------------------------------------------------------------------//
+
+        //List of maps
         server.createContext("/api/maps", (exchange -> {
             String respText = mapper.writeValueAsString(warriors.availableMaps());
             doResponse("GET", exchange, respText);
         }));
 
+        //-------------------------------------------------------------------------//
 
+        //Create game
+        server.createContext("/api/games", (exchange -> {
+            //String respText = mapper.writeValueAsString(game);
+            InputStream input = exchange.getRequestBody();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            //reader.lines().forEach(System.out::println);
+
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            //JSONObject json = new JSONObject(sb.toString());
+            //Game game = mapper.readValue(reader.lines(), Game.class);
+
+
+            //System.out.print(game);
+            //doResponse("POST", exchange, respText);
+            exchange.close();
+        }));
         /////////////////////////////////////////////////////////////////////////////
 
 
